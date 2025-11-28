70bb82bcecb07135aa852351ded570504d5dc5ad
Serigzhan Naviy <serigzhan_naviy@epam.com>
2025-11-28 11:12:38 +0500

    Add project files.

---
+++ UnitTesting/HarryPotter/HarryPotterKataTests.cs
@@ -0,0 +1,23 @@
+﻿namespace UnitTesting.HarryPotter
+{
+    public class HarryPotterKataTests
+    {
+
+        [SetUp]
+        public void SetUp() {}
+
+
+        [Test]
+        public void Should_Return_Zero_If_Cart_Is_Empty()
+        {
+
+            var cart = new ShoppingCart();
+
+            var actualTotal = cart.Total;
+
+            Assert.AreEqual(0, actualTotal);
+
+        }
+
+    }
+}
