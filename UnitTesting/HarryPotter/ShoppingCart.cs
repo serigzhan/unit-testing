316a3c41e2db5e43615f2600e842d4a536b8fd42
Serigzhan Naviy <serigzhan_naviy@epam.com>
2025-11-28 11:45:01 +0500

    Implement adding to cart and sum all book prices

--- UnitTesting/HarryPotter/ShoppingCart.cs
+++ UnitTesting/HarryPotter/ShoppingCart.cs
@@ -1,13 +1,39 @@
 ﻿namespace UnitTesting.HarryPotter
 {
+
+    public record CartItem(string Name, int Quantity, double Price);
+
     public class ShoppingCart
     {
 
-        public double Total { get; } = 0.0;
+        private List<CartItem> _shoppingCart = [];
 
-        public void Add(string bookName, double quantity)
+        public double Total
         {
-            throw new NotImplementedException();
+            get { return Sum(); }
+        }
+
+        public void Add(string bookName, int quantity)
+        {
+            _shoppingCart.Add(new CartItem(bookName, quantity, 8.00));
+        }
+
+        private double Sum()
+        {
+
+            if (_shoppingCart.Count == 0) return 0.00;
+
+            double sum = 0.00;
+
+            foreach (CartItem item in _shoppingCart)
+            {
+
+                sum += item.Price * item.Quantity;
+
+            }
+
+            return sum;
+
         }
 
     }
