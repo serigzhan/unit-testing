5af33d178fdfb55e70c097f34314cd7755d63c79
Serigzhan Naviy <serigzhan_naviy@epam.com>
2025-11-29 20:09:08 +0500

    Implement check each column contains unique numbers

--- UnitTesting/Sudoku/Sudoku.cs
+++ UnitTesting/Sudoku/Sudoku.cs
@@ -11,21 +11,32 @@ namespace UnitTesting.Sudoku
             bool cantContinue = matrix.Any(row => row.Contains(0));
             if (cantContinue) return false;
 
-            cantContinue = HasDuplicateNumbersOnRow(matrix);
+            cantContinue = HasDuplicateNumbersOnRowOrColumn(matrix);
             if(cantContinue) return false;
 
             return true;
 
         }
 
-        private static bool HasDuplicateNumbersOnRow(int[][] matrix)
+        private static bool HasDuplicateNumbersOnRowOrColumn(int[][] matrix)
         {
 
             for (int i = 0;  i < matrix.Length; i++)
             {
-                var row = new HashSet<int>(matrix[i]);
 
+                var row = new HashSet<int>(matrix[i]);
                 if (row.Count != 9) return true;
+
+                var column = new HashSet<int>();
+
+                for (int j = 0; j < matrix.Length; j++)
+                {
+
+                    column.Add(matrix[j][i]);
+
+                }
+
+                if (column.Count != 9) return true;
 
             }
 
