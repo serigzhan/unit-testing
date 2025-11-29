1473a5da42d01c585884c6ff1353e428939ceaef
Serigzhan Naviy <serigzhan_naviy@epam.com>
2025-11-29 19:55:23 +0500

    Implement check each row contains unique numbers

--- UnitTesting/Sudoku/Sudoku.cs
+++ UnitTesting/Sudoku/Sudoku.cs
@@ -8,10 +8,28 @@ namespace UnitTesting.Sudoku
         public static bool ValidSolution(int[][] matrix)
         {
 
-            bool containsZero = matrix.Any(row => row.Contains(0));
-            if (containsZero) return false;
+            bool cantContinue = matrix.Any(row => row.Contains(0));
+            if (cantContinue) return false;
+
+            cantContinue = HasDuplicateNumbersOnRow(matrix);
+            if(cantContinue) return false;
 
             return true;
+
+        }
+
+        private static bool HasDuplicateNumbersOnRow(int[][] matrix)
+        {
+
+            for (int i = 0;  i < matrix.Length; i++)
+            {
+                var row = new HashSet<int>(matrix[i]);
+
+                if (row.Count != 9) return true;
+
+            }
+
+            return false;
 
         }
 
