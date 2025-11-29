b9389ee08774ee8c8fce0ab6320543d0ace1e1a1
Serigzhan Naviy <serigzhan_naviy@epam.com>
2025-11-29 19:34:13 +0500

    Add test case to check empty cells

--- UnitTesting/Sudoku/SudokuTests.cs
+++ UnitTesting/Sudoku/SudokuTests.cs
@@ -1,10 +1,32 @@
-﻿namespace UnitTesting.Sudoku
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+
+namespace UnitTesting.Sudoku
 {
     public class SudokuTests
     {
 
         [SetUp]
         public void SetUp() { }
+
+        [Test]
+        public void Should_Check_Empty_Cells()
+        {
+
+            var actualResult = Sudoku.ValidSolution([
+                [5, 3, 4, 6, 7, 8, 9, 1, 2],
+                [6, 7, 2, 1, 9, 0, 3, 4, 8],
+                [1, 0, 0, 3, 4, 2, 5, 6, 0],
+                [8, 5, 9, 7, 6, 1, 0, 2, 0],
+                [4, 2, 6, 8, 5, 3, 7, 9, 1],
+                [7, 1, 3, 9, 2, 4, 8, 5, 6],
+                [9, 0, 1, 5, 3, 7, 2, 1, 4],
+                [2, 8, 7, 4, 1, 9, 6, 3, 5],
+                [3, 0, 0, 4, 8, 1, 1, 7, 9]
+            ]);
+
+            Assert.That(actualResult, Is.EqualTo(false));
+
+        }
 
     }
 }
