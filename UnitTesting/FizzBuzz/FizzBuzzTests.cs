53c9583bc2c04df36bb4c486d00dc16940211066
Serigzhan Naviy <serigzhan_naviy@epam.com>
2025-11-29 19:04:07 +0500

    Add test case for printing numbers as it is without change

--- UnitTesting/FizzBuzz/FizzBuzzTests.cs
+++ UnitTesting/FizzBuzz/FizzBuzzTests.cs
@@ -5,5 +5,19 @@
 
         [SetUp]
         public void SetUp() { }
+
+        [TestCase(1, "1")]
+        [TestCase(29, "29")]
+        [TestCase(53, "53")]
+        [TestCase(98, "98")]
+        public void Should_Return_Same_Passed_Number(int input, string output)
+        {
+
+            var actualNumber = FizzBuzz.Print(input);
+
+            Assert.That(actualNumber, Is.EqualTo(output));
+
+        }
+
     }
 }
