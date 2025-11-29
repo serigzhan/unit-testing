4f2b5b3debfd880c16380d893a47969ed906f566
Serigzhan Naviy <serigzhan_naviy@epam.com>
2025-11-29 18:34:52 +0500

    Add test case for groupping books to sets for discount

--- UnitTesting/HarryPotter/HarryPotterKataTests.cs
+++ UnitTesting/HarryPotter/HarryPotterKataTests.cs
@@ -46,5 +46,39 @@
 
         }
 
+        [Test]
+        public void Should_Return_With_Max_Discount_For_Five_Different_Books()
+        {
+
+            var cart = new ShoppingCart();
+            cart.Add("Harry Potter and the Sorcerer's Stone", 1);
+            cart.Add("Harry Potter and the Chamber of Secrets", 1);
+            cart.Add("Harry Potter and the Prisoner of Azkaban", 1);
+            cart.Add("Harry Potter and the Goblet of Fire", 1);
+            cart.Add("Harry Potter and the Order of the Phoenix", 1);
+
+            var actualTotal = cart.Total;
+
+            Assert.That(actualTotal, Is.EqualTo(30.00));
+
+        }
+
+        [Test]
+        public void Should_Return_With_Possible_Max_Discount_For_Set_Of_Different_Books()
+        {
+
+            var cart = new ShoppingCart();
+            cart.Add("Harry Potter and the Sorcerer's Stone", 2);
+            cart.Add("Harry Potter and the Chamber of Secrets", 2);
+            cart.Add("Harry Potter and the Prisoner of Azkaban", 2);
+            cart.Add("Harry Potter and the Goblet of Fire", 1);
+            cart.Add("Harry Potter and the Order of the Phoenix", 1);
+
+            var actualTotal = cart.Total;
+
+            Assert.That(actualTotal, Is.EqualTo(51.60));
+
+        }
+
     }
 }
